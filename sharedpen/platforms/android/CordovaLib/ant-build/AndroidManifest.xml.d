d5f68a9f3f5e84a0f5f2424444b956e4935718e7
Nelson Gomes <nelsonmpg@hotmail.com>
2015-05-22 21:28:49 +0100

    Atualização do servidor para a nova localização dos ficheiros.

--- sharedpen/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/apocas/Dropbox/Work/Github/sharedpen/sharedpen/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/apocas/Dropbox/Work/Github/sharedpen/sharedpen/platforms/android/CordovaLib/AndroidManifest.xml \
--- sharedpen/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/apocas/Dropbox/Work/Github/sharedpen/sharedpen/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/Users/apocas/Dropbox/Work/Github/sharedpen/sharedpen/platforms/android/ant-build/CordovaApp.ap_ \
-/Users/apocas/Dropbox/Work/Github/sharedpen/sharedpen/platforms/android/ant-build/classes.dex \
-/Users/apocas/Dropbox/Work/Github/sharedpen/sharedpen/platforms/android/CordovaLib/ant-build/classes.jar \
