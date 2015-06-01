bc969af53d8938214bb310b3c06f5cecc9cd1575
Ricardo Cruz <ricardoc.94911@gmail.com>
2015-06-01 23:52:04 +0100

    Tabelas de gestao de utilizadores agora apresentam o Avatar que diz respeito aos mesmos

--- sharedpen/www/js/scripts.js
+++ sharedpen/www/js/scripts.js
@@ -2155,7 +2155,7 @@ function addLayoutToDiv(local, folder, layout, stk) {
                         for (var i = 0, max = data.length; i < max; i++) {
                             htmlVar+="<tr>";
 							htmlVar += "<td>"+data[i].id_user+"</td>" +
-                                "<td>"+data[i].avatar+"</td>"+
+                                "<td>" + '<img class="text-center avatar-mini" src="' +data[i].avatar +'"></td>'+
 								"<td>"+data[i].username+"</td>" +
                                 "<td>"+data[i].nome_aluno+"</td>" +
                                 "<td>"+data[i].num_aluno+"</td>" +
@@ -2195,7 +2195,7 @@ function addLayoutToDiv(local, folder, layout, stk) {
                         for (var i = 0, max = data.length; i < max; i++) {
                             htmlVar+="<tr>";
 							htmlVar += "<td>"+data[i].id+"</td>" +
-                                "<td>"+data[i].avatar+"</td>"+
+                                "<td>" + '<img class="text-center avatar-mini" src="' +data[i].avatar +'"></td>'+
 								"<td>"+data[i].username+"</td>" +
                                 "<td>"+data[i].nome_professor+"</td>" +
                                 "<td>"+data[i].email+"</td>" +
