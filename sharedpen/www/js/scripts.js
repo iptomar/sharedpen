aedf2edb93bf1f05fc3a128f870aa4551e6e661c
Oleksandr Novak <simbol00@gmail.com>
2015-06-05 11:21:24 +0100

    cleanup

--- sharedpen/www/js/scripts.js
+++ sharedpen/www/js/scripts.js
@@ -29,9 +29,9 @@ var tmpModels = [];
 var currentPosition = 1;
 $(document).ready(function () {
 
-    //backoffice
-
+    //--------------------------BACKOFFICE----------------------------------------
     
+    //fazer update a aluno
     $("body").on('click', "#guardarEditAluno", function (e) {
         e.stopPropagation();
         e.preventDefault();
@@ -60,6 +60,7 @@ $(document).ready(function () {
         });
         });
         
+        //fazer update a professor
         $("body").on('click', "#guardarEditProfessor", function (e) {
         e.stopPropagation();
         e.preventDefault();
@@ -87,6 +88,7 @@ $(document).ready(function () {
         });
         
     
+        //fazer update a escola
         $("body").on('click', "#guardarEditEscola", function (e) {
         e.stopPropagation();
         e.preventDefault();
@@ -112,6 +114,7 @@ $(document).ready(function () {
         });
         });
     
+        //fazer update a agrupamento
         $("body").on('click', "#guardarEditAgrupamento", function (e) {
         e.stopPropagation();
         e.preventDefault();
@@ -200,61 +203,49 @@ $(document).ready(function () {
         var type= $(this).data("type");
         switch(type){
                 
-               case "aluno":
-                
-                    
-                    addLayoutToDiv("#contentor", "html", "EditarAluno.html", socket);
+            case "aluno":      
+                addLayoutToDiv("#contentor", "html", "EditarAluno.html", socket);
                 if (currentPosition != backArray.length) {
-            backArray.splice(currentPosition, backArray.length - currentPosition);
-            folderArray.splice(currentPosition, folderArray.length - currentPosition);
-        }
-
-        backArray.push($(this).data("layout"));
-        folderArray.push($(this).data("folder"));
-
-        currentPosition += 1;
-                    $("body").append(wait);
-                    $.ajax({
-                        type: "GET",
-                        url: "/getAluno/" + $(this).attr("rel"),
-                        dataType: 'json',
-                        success: function (data) {
-                            var htmlVar;
-                            //$("#userImage").attr('src', data[0].avatar);
-                            $("#Id_aluno_edit").val(data[0].id_user);
-                            $("#username_aluno_edit").val(data[0].username);
-                            $("#nome_aluno_edit").val(data[0].nome_aluno);
-                            $("#numero_aluno_edit").val(data[0].num_aluno);
-                            $("#turma_aluno_edit").val(data[0].turma);
-                            $("#ano_aluno_edit").val(data[0].ano);
-                            $("#escola_aluno_edit").append(
-                                $('<option></option>').val(data[0].nome_escola).html(data[0].nome_escola));
-
-                            $("body").find("#loading").remove();
-
-                        },
-                        error: function (error) {
-                            $("body").find("#loading").remove();
-                            alert("Erro ao tentar carregar os dados para paginas.\nTente Novamente.")
-                            console.log(JSON.stringify(error));
-                        }
-                    });
-                    
-                break;
-                
-                
-                case "professor":
-                
-                    
-                    addLayoutToDiv("#contentor", "html", "EditarProfessor.html", socket);
-                    if (currentPosition != backArray.length) {
-                        backArray.splice(currentPosition, backArray.length - currentPosition);
-                        folderArray.splice(currentPosition, folderArray.length - currentPosition);
-                    }
-
+                    backArray.splice(currentPosition, backArray.length - currentPosition);
+                    folderArray.splice(currentPosition, folderArray.length - currentPosition);
+                }
                 backArray.push($(this).data("layout"));
                 folderArray.push($(this).data("folder"));
-
+                currentPosition += 1;
+                $("body").append(wait);
+                $.ajax({
+                    type: "GET",
+                    url: "/getAluno/" + $(this).attr("rel"),
+                    dataType: 'json',
+                    success: function (data) {
+                        var htmlVar;
+                        //$("#userImage").attr('src', data[0].avatar);
+                        $("#Id_aluno_edit").val(data[0].id_user);
+                        $("#username_aluno_edit").val(data[0].username);
+                        $("#nome_aluno_edit").val(data[0].nome_aluno);
+                        $("#numero_aluno_edit").val(data[0].num_aluno);
+                        $("#turma_aluno_edit").val(data[0].turma);
+                        $("#ano_aluno_edit").val(data[0].ano);
+                        $("#escola_aluno_edit").append(
+                        $('<option></option>').val(data[0].nome_escola).html(data[0].nome_escola));
+                        $("body").find("#loading").remove();
+                    },
+                    error: function (error) {
+                        $("body").find("#loading").remove();
+                        alert("Erro ao tentar carregar os dados para paginas.\nTente Novamente.")
+                        console.log(JSON.stringify(error));
+                    }
+                });
+                break;
+                
+            case "professor":
+                addLayoutToDiv("#contentor", "html", "EditarProfessor.html", socket);
+                if (currentPosition != backArray.length) {
+                    backArray.splice(currentPosition, backArray.length - currentPosition);
+                    folderArray.splice(currentPosition, folderArray.length - currentPosition);
+                }
+                backArray.push($(this).data("layout"));
+                folderArray.push($(this).data("folder"));
                 currentPosition += 1;
                 $("body").append(wait);
                 $.ajax({
@@ -269,10 +260,7 @@ $(document).ready(function () {
                         $("#Nome_Professor_edit").val(data[0].nome_professor);
                         $("#Agrupamento_Professor_edit").val(data[0].id_agrupamento);
                         $("#Email_Professor_edit").val(data[0].email);
-                        
-                        
                         $("body").find("#loading").remove();
-                        
                     },
                     error: function (error) {
                         $("body").find("#loading").remove();
@@ -280,24 +268,16 @@ $(document).ready(function () {
                         console.log(JSON.stringify(error));
                     }
                 });
-                    
                 break; 
-                
-                
-                
-                
-                case "escola":
-                
-                    
-                    addLayoutToDiv("#contentor", "html", "EditarEscola.html", socket);
-                    if (currentPosition != backArray.length) {
-                        backArray.splice(currentPosition, backArray.length - currentPosition);
-                        folderArray.splice(currentPosition, folderArray.length - currentPosition);
-                    }
 
+            case "escola":
+                addLayoutToDiv("#contentor", "html", "EditarEscola.html", socket);
+                if (currentPosition != backArray.length) {
+                    backArray.splice(currentPosition, backArray.length - currentPosition);
+                    folderArray.splice(currentPosition, folderArray.length - currentPosition);
+                }
                 backArray.push($(this).data("layout"));
                 folderArray.push($(this).data("folder"));
-
                 currentPosition += 1;
                 $("body").append(wait);
                 $.ajax({
@@ -312,10 +292,7 @@ $(document).ready(function () {
                         $("#Morada_Escola_edit").val(data[0].morada);
                         $("#Contacto_Escola_edit").val(data[0].contacto);
                         $("#Agrupamento_Escola_edit").val(data[0].id_agrupamento);
-
-                        
                         $("body").find("#loading").remove();
-                        
                     },
                     error: function (error) {
                         $("body").find("#loading").remove();
@@ -323,21 +300,16 @@ $(document).ready(function () {
                         console.log(JSON.stringify(error));
                     }
                 });
-                    
                 break; 
                 
-                 case "agrupamento":
-                
-                    
-                    addLayoutToDiv("#contentor", "html", "EditarAgrupamento.html", socket);
-                    if (currentPosition != backArray.length) {
-                        backArray.splice(currentPosition, backArray.length - currentPosition);
-                        folderArray.splice(currentPosition, folderArray.length - currentPosition);
-                    }
-
+            case "agrupamento":
+                addLayoutToDiv("#contentor", "html", "EditarAgrupamento.html", socket);
+                if (currentPosition != backArray.length) {
+                    backArray.splice(currentPosition, backArray.length - currentPosition);
+                    folderArray.splice(currentPosition, folderArray.length - currentPosition);
+                }
                 backArray.push($(this).data("layout"));
                 folderArray.push($(this).data("folder"));
-
                 currentPosition += 1;
                 $("body").append(wait);
                 $.ajax({
@@ -349,10 +321,7 @@ $(document).ready(function () {
                         //$("#userImage").attr('src', data[0].avatar);
                         $("#Id_Agrupamento_edit").val(data[0].id);
                         $("#Nome_Agrupamento_edit").val(data[0].nome);
-    
-                   
                         $("body").find("#loading").remove();
-                        
                     },
                     error: function (error) {
                         $("body").find("#loading").remove();
@@ -360,12 +329,8 @@ $(document).ready(function () {
                         console.log(JSON.stringify(error));
                     }
                 });
-                    
                 break; 
         }
-            
-        
-        
     });
 
     $.ajaxSetup({async: false});
@@ -2562,6 +2527,7 @@ function addLayoutToDiv(local, folder, layout, stk) {
                 });
 
                 break;
+                
             case "GerirAgrupamentos.html":
                 $("body").append(wait);
                 $.ajax({
